95577c376eb20340e79ed7c79e34c05f90163dcf
derekaj16 <dprodigy@byu.edu>
2023-04-10 16:47:49 -0600

    hello

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mummies.Data;
+using mummies.Models;
+
+namespace mummies.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    private ApplicationDbContext mummyContext;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        mummyContext = context;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.things = mummyContext.Burialmain.ToList();
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult BurialInfo()
+    {
+        return View();
+    }
+
+    public IActionResult SupervisedAnalysis()
+    {
+        return View();
+    }
+
+    public IActionResult UnsupervisedAnalysis()
+    {
+        return View();
+    }
+
+    public IActionResult Account()
+    {
+        return View();
+    }
+
+    public IActionResult ManageAccounts()
+    {
+        return View();
+    }
+
+    public IActionResult BurialDetails()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mummies.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mummies.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Burialmain> Burialmain { get; set; }
+    }
+}
