54ec49178a0d866f748e8a7b6bd3c080488d1f48
Henry Matsen <hdm32@byu.edu>
2023-04-11 16:19:19 -0600

    Crud

---
+++ Models/ViewModels/MummyViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Mummies.Models;
+
+namespace mummies.Models.ViewModels
+{
+    public class MummyViewModel
+    {
+        public IQueryable<Burialmain> Burial { get; set; }
+        public IQueryable<BurialmainTextile> burialmainTextiles { get; set; }
+        public IQueryable<Textile> Textile { get; set; }
+        public IQueryable<TextilefunctionTextile> textilefunctionTextiles { get; set; }
+        public IQueryable<Textilefunction> textilefunctions { get; set; }
+        public IQueryable<StructureTextile> structureTextiles { get; set; }
+        public IQueryable<Structure> structures { get; set; }
+        public IQueryable<ColorTextile> colorTextiles { get; set; } 
+        public IQueryable<Color> colors { get; set; }
+
+        
+    }
+}
